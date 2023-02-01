7ee08b47460d68b05547caeee11bd4af09c1b3df
bahadirersin <bahadirersin@gmail.com>
2023-02-01 22:41:31 +0300

    Initial Commit

---
+++ Moonshot/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Bahadır Ersin on 1.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
